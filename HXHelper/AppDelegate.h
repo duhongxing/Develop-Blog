14614f4b46cb698f1a2d65a1bc1d19cd9d13626f
13821963975@163.com <13821963975@163.com>
2017-08-12 13:52:56 +0800

    first commit

---
+++ HXHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HXHelper
+//
+//  Created by MacOS on 17/8/12.
+//  Copyright © 2017年 baitongshiji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
